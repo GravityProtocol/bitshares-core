9e2fe4a8a1f7fe5ac5932dc6dded3f5fac45b9ff
Unknown <petr.kotegov@gmail.com>
2018-07-31 17:09:00 +0300

    Removed singularity library copy

--- libraries/singularity/include/graphene/singularity/singularity.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef SINGULARITY_HPP
-#define SINGULARITY_HPP
-
-#include <graphene/singularity/emission.hpp>
-#include <graphene/singularity/gravity_index_calculator.hpp>
-#include <graphene/singularity/activity_index_calculator.hpp>
-
-#endif
